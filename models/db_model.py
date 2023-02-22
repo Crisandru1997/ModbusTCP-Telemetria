e1cc6039d38b7f9522c7392715d3855191fb8805
Cristian <crisandru1997@gmail.com>
2023-02-22 16:03:48 -0300

    quitando la base de datossss

--- models/db_model.py
+++ models/db_model.py
@@ -1,17 +1,17 @@
-from flask_sqlalchemy import SQLAlchemy
-import datetime
+#from flask_sqlalchemy import SQLAlchemy
+#import datetime
 
-db = SQLAlchemy()
+#db = SQLAlchemy()
 
-class Temperature(db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary_key=True es para que el id sea autoincrementable.
-    temperature = db.Column(db.Float) # db.Float es para que la temperatura sea un número decimal.
-    timestamp = db.Column(db.DateTime) # db.DateTime es para que la fecha sea un formato de fecha y hora.
-
-    def __init__(self, temperature, timestamp=None):
-        self.temperature = temperature
-        # Si no se especifica la fecha y hora, se toma la fecha y hora actual.
-        if timestamp is None:
-            timestamp = datetime.utcnow()
-        # Se guarda la fecha y hora en el atributo timestamp.
-        self.timestamp = timestamp
+#class Temperature(db.Model):
+#    id = db.Column(db.Integer, primary_key=True) # primary_key=True es para que el id sea autoincrementable.
+#    temperature = db.Column(db.Float) # db.Float es para que la temperatura sea un número decimal.
+#    timestamp = db.Column(db.DateTime) # db.DateTime es para que la fecha sea un formato de fecha y hora.
+#
+#    def __init__(self, temperature, timestamp=None):
+#        self.temperature = temperature
+#        # Si no se especifica la fecha y hora, se toma la fecha y hora actual.
+#        if timestamp is None:
+#            timestamp = datetime.utcnow()
+#        # Se guarda la fecha y hora en el atributo timestamp.
+#        self.timestamp = timestamp
